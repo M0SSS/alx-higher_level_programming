fca724f110e07ac1dfe8df08d0f8443d52fe9391
Musse <114333950+M0SSS@users.noreply.github.com>
2023-04-28 18:49:55 -0700

    email post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Sends a POST request to a given URL with a given email.
 Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
+- Displays the body of the response."""
 import sys
 import urllib.parse
 import urllib.request
